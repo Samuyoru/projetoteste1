6ae03057215cf1d36689fc6c66ec9fa16e67c9e1
Samuyoru <samuyoru@gmail.com>
2025-01-08 19:19:04 -0300

    Add files via upload

---
+++ estudopraticohtmlcss/inicio.js
@@ -0,0 +1,9 @@
+const produto1 = document.getElementById('tenisnike');
+produto1.addEventListener('click', () => {
+    produto1.classList.toggle('produto1');
+}); 
+
+const produto2 = document.getElementById('alexa');
+produto2.addEventListener('click', () => {
+  produto2.classList.toggle('produto2');
+}); 
